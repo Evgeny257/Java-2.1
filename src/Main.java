7a1b47f23a34650c7860e7d937b360fee59401d6
Eugene <galanin.zhenia@yandex.ru>
2023-03-30 22:17:16 +0300

    prog

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int cost = 14500;
+        int rub = 20;
+        int bonus = cost / rub;
+        System.out.println("Your miles = " + bonus);
     }
 }
